eef64ccc09d0bf64d7802c3a46cff98a2084eae0
Balaj Marius <balajmarius@gmail.com>
2021-06-01 13:35:19 +0300

    Safari fix

--- src/core/Controller.tsx
+++ src/core/Controller.tsx
@@ -1,7 +1,7 @@
 import React, { MutableRefObject, ReactNode, useCallback, useRef, useState } from "react"
 import downloadjs from "downloadjs"
 import { update } from "ramda"
-import { toCanvas, toPng } from "html-to-image"
+import { toBlob, toCanvas, toPng } from "html-to-image"
 import { IPoint } from "face-api.js"
 
 import { ACTIVE_MASK_DEFAULT, CONTROLLER_SCALE_DEFAULT, FACE_DEFAULT } from "../helpers/const"
@@ -85,6 +85,7 @@ export const ControllerProvider: React.FC<Props> = ({ children }: Props) => {
     if (faceRef?.current && artboardRef?.current) {
       try {
         await toCanvas(artboardRef.current)
+        await toBlob(artboardRef.current)
         const source = await toPng(artboardRef.current, {
           skipFonts: true,
           canvasWidth: faceRef.current.naturalWidth,
